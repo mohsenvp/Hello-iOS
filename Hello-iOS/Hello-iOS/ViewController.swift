3500f4c9be3276794e65eea88dfaeb744281da88
Mohsen <m.sedaghatfard@gmail.com>
2022-02-08 22:45:09 +0330

    Init Vesile project

---
+++ Hello-iOS/Hello-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by Mohsen on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
